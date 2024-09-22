2b9dafc6531c60a0d13b0ae9edef44e31ed53729
lecav30 <lecav30@outlook.com>
2024-09-22 17:05:53 -0500

    feat(header-auth): component without refs

--- src/app/shared/components/auth/header-auth/header-auth.component.ts
+++ src/app/shared/components/auth/header-auth/header-auth.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header-auth',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header-auth.component.html',
-  styleUrl: './header-auth.component.css'
+  styleUrl: './header-auth.component.css',
 })
-export class HeaderAuthComponent {
-
-}
+export class HeaderAuthComponent {}
