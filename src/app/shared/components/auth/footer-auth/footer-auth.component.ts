35ea7a864f4dfe2b40db1ccaaad6ec02d1781ee1
lecav30 <lecav30@outlook.com>
2024-09-22 15:34:24 -0500

    refactor(layout): create auth components

---
+++ src/app/shared/components/auth/footer-auth/footer-auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-auth.component.html',
+  styleUrl: './footer-auth.component.css'
+})
+export class FooterAuthComponent {
+
+}
