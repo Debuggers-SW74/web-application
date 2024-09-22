1580683789d646f63540d254c4663e1e16c54281
lecav30 <lecav30@outlook.com>
2024-09-22 15:05:39 -0500

    feat: authentication routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sign-in',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign-in',
+    //title: 'Sign In',
+    loadComponent: () =>
+      import('./public/pages/sign-in/sign-in.component').then(
+        (m) => m.SignInComponent,
+      ),
+  },
+  {
+    path: 'sign-up',
+    //title: 'Sign Up',
+    loadComponent: () =>
+      import('./public/pages/sign-up/sign-up.component').then(
+        (m) => m.SignUpComponent,
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'sign-in',
+  },
+];
