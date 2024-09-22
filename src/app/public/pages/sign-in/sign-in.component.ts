ba8c0308882a92d6acc12288b060833c378697b0
lecav30 <lecav30@outlook.com>
2024-09-22 18:50:42 -0500

    feat(sign-in): ui design

--- src/app/public/pages/sign-in/sign-in.component.ts
+++ src/app/public/pages/sign-in/sign-in.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {
-
-}
+export class SignInComponent {}
