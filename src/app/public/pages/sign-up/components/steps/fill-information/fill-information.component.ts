923c39a5eddec6716a32270172cc1497cec35c61
lecav30 <lecav30@outlook.com>
2024-09-23 01:05:20 -0500

    feat(fill information): render fill information step in sign-up

--- src/app/public/pages/sign-up/components/steps/fill-information/fill-information.component.ts
+++ src/app/public/pages/sign-up/components/steps/fill-information/fill-information.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fill-information',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule, MatCheckboxModule],
   templateUrl: './fill-information.component.html',
   styleUrl: './fill-information.component.css'
 })
 export class FillInformationComponent {
+  fillInformationForm!: FormGroup;
 
+  constructor(
+    private formBuilder:FormBuilder,
+    private router:Router
+  ) {}
+
+  submit() {
+    console.log('Fill Information Submitted');
+    this.router.navigate(['/home']);
+  }
+
+  ngOnInit(): void {
+    this.fillInformationForm = this.formBuilder.group({
+      name: [''],
+      phoneNumber: [''],
+      termsConditions: [false],
+      information: [false],
+    })
+  }
 }
