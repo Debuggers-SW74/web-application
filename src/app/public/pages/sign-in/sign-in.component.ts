f449b605ab6c95b4d6fb8c8ea1f26f9376d27c24
lecav30 <lecav30@outlook.com>
2024-09-22 21:37:21 -0500

    feat(sign-in): button functionalities

--- src/app/public/pages/sign-in/sign-in.component.ts
+++ src/app/public/pages/sign-in/sign-in.component.ts
@@ -2,12 +2,33 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, MatButtonModule],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule, RouterModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {}
+export class SignInComponent {
+
+  signInForm!: FormGroup;
+
+  constructor(
+    private router:Router,
+    private formBuilder:FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.signInForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  signIn() {
+    this.router.navigate(['/home']);
+  }
+}
