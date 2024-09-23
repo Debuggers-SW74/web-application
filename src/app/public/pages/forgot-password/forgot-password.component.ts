2ed050d5fae4e8fb1eaf8af6a00445caf4f4463e
lecav30 <lecav30@outlook.com>
2024-09-23 01:23:59 -0500

    feat(forgot-password): render steps

--- src/app/public/pages/forgot-password/forgot-password.component.ts
+++ src/app/public/pages/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { ForgotPasswordSteps } from '@app/shared/models/forgot-password-steps';
+import { SendEmailComponent } from './components/steps/send-email/send-email.component';
+import { NewPasswordComponent } from './components/steps/new-password/new-password.component';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,4 +12,41 @@ import { Component } from '@angular/core';
 })
 export class ForgotPasswordComponent {
 
+  steps = [
+    ForgotPasswordSteps.SendEmail,
+    ForgotPasswordSteps.NewPassword,
+  ];
+
+  currentStep = this.steps[0];
+  currentStepView: any = SendEmailComponent;
+
+  @ViewChild('dynamicComponentContainer', { read: ViewContainerRef, static:true})
+  container!: ViewContainerRef;
+
+  constructor() { }
+
+  changeStep(step: ForgotPasswordSteps) {
+    console.log(step);
+    this.currentStep = step;
+    if (step === ForgotPasswordSteps.SendEmail) {
+      this.currentStepView = SendEmailComponent;
+    }
+    if (step === ForgotPasswordSteps.NewPassword) {
+      this.currentStepView = NewPasswordComponent;
+    }
+    this.loadComponent(this.currentStepView);
+  }
+
+  loadComponent(component: any) {
+    this.container.clear();
+    const componentRef = this.container.createComponent(component);
+
+    if (componentRef.instance instanceof SendEmailComponent) {
+      componentRef.instance.onSubmit = () => this.changeStep(this.steps[1]);
+    }
+  }
+
+  ngOnInit(): void {
+    this.loadComponent(this.currentStepView);
+  }
 }
