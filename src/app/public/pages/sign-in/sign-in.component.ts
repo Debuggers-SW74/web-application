c1fd60cc1dc8309de27d9da1681767b19f8fbf37
lecav30 <lecav30@outlook.com>
2024-09-23 20:47:10 -0500

    feat(sign-in): form

--- src/app/public/pages/sign-in/sign-in.component.ts
+++ src/app/public/pages/sign-in/sign-in.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { TitleComponent } from '@shared/components/auth/title/title.component';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
@@ -19,19 +25,25 @@ import { TitleComponent } from '@shared/components/auth/title/title.component';
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
   signInForm!: FormGroup;
 
   constructor(private router: Router, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.signInForm = this.formBuilder.group({
-      email: [''],
-      password: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
     });
   }
 
-  signIn() {
+  signIn(): void {
+    if (this.signInForm.invalid) {
+      alert('Invalid form, please fill all fields correctly');
+      return;
+    }
+
+    console.log('Form is valid, navigating to home');
     this.router.navigate(['/home']);
   }
 }
