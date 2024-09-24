153794215a659763c206a038457fa3b202970a76
lecav30 <lecav30@outlook.com>
2024-09-23 21:27:12 -0500

    feat(sign-in): use user service

--- src/app/public/pages/sign-in/sign-in.component.ts
+++ src/app/public/pages/sign-in/sign-in.component.ts
@@ -3,6 +3,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from '@shared/services/user/user.service';
+import { User } from '@app/shared/models/entities/User';
 import {
   FormBuilder,
   FormGroup,
@@ -21,14 +24,20 @@ import { TitleComponent } from '@shared/components/auth/title/title.component';
     MatButtonModule,
     RouterModule,
     TitleComponent,
+    HttpClientModule,
   ],
+  providers: [UserService],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
 export class SignInComponent implements OnInit {
   signInForm!: FormGroup;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.signInForm = this.formBuilder.group({
@@ -43,7 +52,17 @@ export class SignInComponent implements OnInit {
       return;
     }
 
-    console.log('Form is valid, navigating to home');
-    this.router.navigate(['/home']);
+    const { email, password } = this.signInForm.value;
+
+    this.userService.signIn(email, password).subscribe(
+      (user: User) => {
+        console.log('User signed in successfully:', user);
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        console.log('Error signing in:', error);
+        alert('Error signing in, please try again later');
+      }
+    );
   }
 }
