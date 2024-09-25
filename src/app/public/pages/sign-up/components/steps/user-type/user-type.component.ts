b592dcb94307c9d5576174b3666d216d4eac5487
lecav30 <lecav30@outlook.com>
2024-09-25 10:01:15 -0500

    feat(user-type): selection

--- src/app/public/pages/sign-up/components/steps/user-type/user-type.component.ts
+++ src/app/public/pages/sign-up/components/steps/user-type/user-type.component.ts
@@ -1,18 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { Role } from '@shared/models/enum/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-type',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './user-type.component.html',
   styleUrl: './user-type.component.css',
 })
 export class UserTypeComponent {
   @Input() onSubmit!: () => void;
 
+  userType: Role | null = null;
+
+  public Role = Role;
+
   submit() {
+    if (this.userType === null) {
+      alert('Please select your type of profile!');
+      return;
+    }
+
     console.log('User Type Submitted');
     this.onSubmit();
+  }
+
+  selectUserType(userType: Role) {
+    this.userType = userType;
   }
 }
