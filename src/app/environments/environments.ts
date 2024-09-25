c0cf48a9342912309f08c68c78f68f8009cf2280
lecav30 <lecav30@outlook.com>
2024-09-25 15:21:33 -0500

    refactor(json): db.json update

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://my-json-server.typicode.com/Debuggers-SW74/dbiot/',
-  apiUrl: 'http://localhost:3000/', // JSON Server
+  apiUrl: 'https://my-json-server.typicode.com/Debuggers-SW74/dbiot/',
+  // apiUrl: 'http://localhost:3000/', // JSON Server
 };
