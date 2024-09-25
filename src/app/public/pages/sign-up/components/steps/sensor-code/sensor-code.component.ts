30992c7cfdf4e23f3fad1df46ccc75cab55a1029
lecav30 <lecav30@outlook.com>
2024-09-25 00:24:29 -0500

    feat(sensor-code): add form group

--- src/app/public/pages/sign-up/components/steps/sensor-code/sensor-code.component.ts
+++ src/app/public/pages/sign-up/components/steps/sensor-code/sensor-code.component.ts
@@ -1,25 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { AutoTabDirective } from '@shared/lib/auto-tab.directive';
 
 @Component({
   selector: 'app-sensor-code',
   standalone: true,
-  imports: [AutoTabDirective, MatButtonModule],
+  imports: [AutoTabDirective, MatButtonModule, ReactiveFormsModule],
   templateUrl: './sensor-code.component.html',
   styleUrl: './sensor-code.component.css',
 })
-export class SensorCodeComponent {
+export class SensorCodeComponent implements OnInit {
   @Input() onSubmit!: () => void;
+  sensorCodeForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.sensorCodeForm = this.formBuilder.group({
+      input1: ['', [Validators.required]],
+      input2: ['', [Validators.required]],
+      input3: ['', [Validators.required]],
+      input4: ['', [Validators.required]],
+      input5: ['', [Validators.required]],
+      input6: ['', [Validators.required]],
+    });
+  }
+
   submit() {
-    const inputs = [];
-    for (let i = 1; i <= 6; i++) {
-      const inputElement = (
-        document.getElementById(`input${i}`) as HTMLInputElement
-      ).value;
-      inputs.push(inputElement);
+    if (this.sensorCodeForm.invalid) {
+      alert('Invalid code, please fill all fields correctly');
+      return;
     }
-    const enteredCode = inputs.join('');
+
+    const enteredCode = Object.values(this.sensorCodeForm.value).join('');
+
     console.log('Entered Code:', enteredCode);
     this.onSubmit();
   }
