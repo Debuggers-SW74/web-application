63243d052d94bb8d485d6e37a1f346c9b5950922
lecav30 <lecav30@outlook.com>
2024-09-25 11:18:11 -0500

    feat(interface): user interfaces

--- src/app/shared/models/entities/User.ts
+++ src/app/shared/models/entities/User.ts
@@ -2,9 +2,22 @@ import { Role } from '../enum/role';
 
 export interface User {
   id: number;
-  firstName: string;
-  lastName: string;
+  name: string;
+  phoneNumber: string;
   email: string;
   password: string;
   role: Role;
+}
+
+export interface UserRegistration {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface UserInformation {
+  name: string;
+  phoneNumber: string;
+  termsConditions: boolean;
+  information: boolean;
 }
