97597c2ae853f1073ff4bd0dce67b010d5ee2d92
lecav30 <lecav30@outlook.com>
2024-09-26 20:25:55 -0500

    feat(drivers): create book trip component

---
+++ src/app/context/transport-management/components/book-trip/book-trip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-trip',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-trip.component.html',
+  styleUrl: './book-trip.component.css'
+})
+export class BookTripComponent {
+
+}
