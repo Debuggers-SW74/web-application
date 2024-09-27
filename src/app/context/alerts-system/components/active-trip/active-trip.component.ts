437f5f47349f65458a5e83206b0105e39e245ec2
Eduardo Chero <102326120+EChero11@users.noreply.github.com>
2024-09-27 12:52:51 -0500

    feat (dashboard): ts

--- src/app/context/alerts-system/components/active-trip/active-trip.component.ts
+++ src/app/context/alerts-system/components/active-trip/active-trip.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { ChartConfiguration, ChartData, ChartOptions } from 'chart.js';
+import {BaseChartDirective} from "ng2-charts";
 
 @Component({
   selector: 'app-active-trip',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, BaseChartDirective],
   templateUrl: './active-trip.component.html',
   styleUrl: './active-trip.component.css'
 })
@@ -16,4 +18,84 @@ export class ActiveTripComponent {
     img: "https://upload.wikimedia.org/wikipedia/commons/b/bf/Foto_Perfil_.jpg",
     phone: "987654321"
   }
+
+  public lineChartData: ChartData<'line'> = {
+    labels: Array.from({ length: 24 }, (_, i) => `${i}:00`), 
+    datasets: [
+      {
+        label: 'Temperatura (°C)',
+        data: [15, 16, 17, 18, 19, 21, 23, 25, 27, 29, 30, 31, 32, 31, 30, 29, 27, 26, 24, 22, 20, 19, 18, 16],
+        borderColor: '#ff6384',
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        fill: false,
+      },
+      {
+        label: 'Humedad (%)',
+        data: [40, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40],
+        borderColor: '#36a2eb',
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        stepped: true,
+        yAxisID: 'y2',
+      },
+    ],
+  };
+
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: 'Temperatura (°C)',
+        },
+      },
+      y2: {
+        type: 'category',
+        labels: ['0%', '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'],
+        position: 'right',
+        title: {
+          display: true,
+          text: 'Humedad (%)',
+        },
+      },
+      x: {
+        title: {
+          display: true,
+          text: 'Hora del día',
+        },
+      },
+    },
+  };
+
+  public lineChartType: ChartConfiguration<'line'>['type'] = 'line';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [' '], 
+    datasets: [
+      {
+        label: 'GNV',
+        data: [350], 
+        borderColor: '#36a2eb',
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+        borderWidth: 2,
+        borderRadius: Number.MAX_VALUE,
+        borderSkipped: false,
+      }
+    ],
+  };
+
+  public barChartOptions: ChartOptions<'bar'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Presión de Gas (GNV)',
+      },
+    },
+  };
+
+  public barChartType: ChartConfiguration<'bar'>['type'] = 'bar';
 }
