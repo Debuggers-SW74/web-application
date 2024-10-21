44373e7103b8018bc504e45cf920f2681eb5012a
lecav30 <lecav30@outlook.com>
2024-10-20 21:27:56 -0500

    feat: resume trip card advance

--- src/app/context/transport-management/components/resume-trip-card/resume-trip-card.component.ts
+++ src/app/context/transport-management/components/resume-trip-card/resume-trip-card.component.ts
@@ -1,16 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { Trip } from '@shared/models/entities/Trip';
 import { TripStatus } from '@shared/models/enum/trip-status';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume-trip-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './resume-trip-card.component.html',
   styleUrl: './resume-trip-card.component.css',
 })
 export class ResumeTripCardComponent {
   @Input() trip!: Trip;
   @Input() action!: boolean;
+
+  finishTrip() {
+    console.log('Finish trip');
+  }
+
+  cancelTrip() {
+    console.log('Cancel trip');
+  }
 }
