3fcdf898fd5d81689eca7e5a06199227f66ca8d0
lecav30 <lecav30@outlook.com>
2024-10-31 15:16:48 -0500

    fix(environments): develop and prod

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -4,5 +4,4 @@ export const environment = {
   // apiUrl: 'http://localhost:3000/', // JSON Server
   authUrl: 'http://localhost:8080/authenticate', // Backend local
   apiUrl: 'http://localhost:8080/api/', // Backend local
-  // apiUrl: 'https://fastporte-cloud-api.azurewebsites.net', // Despliegue
 };
