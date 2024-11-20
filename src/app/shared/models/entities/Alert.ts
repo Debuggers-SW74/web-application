667d9ad99168bc8ffc5bc76e5d38d59ef65e963a
lecav30 <lecav30@outlook.com>
2024-11-20 10:22:27 -0500

    fix: alert receive

--- src/app/shared/models/entities/Alert.ts
+++ src/app/shared/models/entities/Alert.ts
@@ -4,3 +4,10 @@ export interface Alert {
   timestamp: string;
   tripId: number;
 }
+
+export interface AlertReceive {
+  id: number;
+  alertLevel: string;
+  message: string;
+  sensorData: Alert;
+}
